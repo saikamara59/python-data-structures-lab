6028d045e293bb54fa098b67cdd3a4885f32be5b
Saidu Kamara <mhsaidu@gmail.com>
2025-04-07 09:30:33 -0400

    practicing more linked list problems

--- linked_list.py
+++ linked_list.py
@@ -1,44 +1,58 @@
-class Node:
-    """"
-    "An object for storing a single node a linked list."
-    "Models two attritubutes - data and the lnik to the next node in the list
-    """
+# class Node:
+#     """"
+#     "An object for storing a single node a linked list."
+#     "Models two attritubutes - data and the lnik to the next node in the list
+#     """
 
-    data = None
-    next_node = None 
+#     data = None
+#     next_node = None 
 
-    def __init__(self,data):
-        self.data = data
+#     def __init__(self,data):
+#         self.data = data
 
 
-    def __repr__(self):
-      return "<Node data: %s>" % self.data
+#     def __repr__(self):
+#       return "<Node data: %s>" % self.data
     
-class LinkedList:
-#   Singly Linked List
+# class LinkedList:
+# #   Singly Linked List
 
-   def __init__(self):
-      self.head = None   
+#    def __init__(self):
+#       self.head = None   
 
-   def is_empty(self):
-      return self.head == None
+#    def is_empty(self):
+#       return self.head == None
    
-   def size(self):
-    #  Returns the number of nodes in the list Takes 0(n) time
-     current = self.head
-     count = 0
+#    def size(self):
+#     #  Returns the number of nodes in the list Takes 0(n) time
+#      current = self.head
+#      count = 0
 
-     while current:
-        count += 1
-        current = current.next_node
+#      while current:
+#         count += 1
+#         current = current.next_node
 
-     return count 
+#      return count 
    
 
 
 
-   def add(self,data):
-   # Adds a new node containing data at the head of the list Takes 0(1) time.
-     new_node = Node(data)
-     new_node.next_node = self.head
-     self.head = new_node
+#    def add(self,data):
+#    # Adds a new node containing data at the head of the list Takes 0(1) time.
+#      new_node = Node(data)
+#      new_node.next_node = self.head
+#      self.head = new_node
+
+class node:
+    def __init__(self,data=None):
+        self.data=data
+        self.data=None
+
+
+class linked_list:
+    def __init__(self):
+        self.head = node()
+
+    def append(self,data):
+        new_node = node(data)
+        cur = self.head
